c166519848bbccb4271c3cce3ae4966433a6ee71
franckdon <frk1990@hotmail.com>
2023-09-29 18:46:15 +0000

    Creation de la structure

--- main.py
+++ main.py
@@ -7,7 +7,63 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
 
+class TitanicDataProcessor:
+    def __init__(self, train_file, test_file):
+        self.train_data = pd.read_csv(train_file)
+        self.test_data = pd.read_csv(test_file)
+    def preprocess_data(self):
+        self._feature_engineering()
+        # self._encode_categorical_features()
+        # self._drop_unused_columns()
+        # self._fill_missing_values()
+        # self._feature_scaling()
+    
+    def train_model(self):
+        X = self.train_data.drop('Survived', axis=1)
+        y = self.train_data['Survived']
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+        
+        clf = RandomForestClassifier(n_estimators=100, random_state=42)
+        clf.fit(X_train, y_train)
+        
+        y_pred = clf.predict(X_val)
+        accuracy = accuracy_score(y_val, y_pred)
+        print(f'Accuracy: {accuracy}')
+        
+        return clf
+    
+    def train_model(self):
+        X = self.train_data.drop('Survived', axis=1)
+        y = self.train_data['Survived']
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+        
+        clf = RandomForestClassifier(n_estimators=100, random_state=42)
+        clf.fit(X_train, y_train)
+        
+        y_pred = clf.predict(X_val)
+        accuracy = accuracy_score(y_val, y_pred)
+        print(f'Accuracy: {accuracy}')
+        
+        return clf
+    
+    def _feature_engineering(self, data=None):
+        if data is None:
+            data = self.train_data
+        data['Title'] = data['Name'].str.extract(' ([A-Za-z]+)\.', expand=False)
+        bins = [0, 18, 35, 60, np.inf]
+        labels = ['Child', 'Young Adult', 'Adult', 'Senior']
+        data['AgeGroup'] = pd.cut(data['Age'], bins, labels=labels)
+        
+        # Exemple 3 : Création de caractéristiques familiales
+        data['FamilySize'] = data['SibSp'] + data['Parch']
+        data['IsAlone'] = 0
+        data.loc[data['FamilySize'] == 0, 'IsAlone'] = 1
+    
+        return data
+    
+    
 class ProjectInitializer:
     def __init__(self):
         self.project_root = os.getcwd()
@@ -42,10 +98,18 @@ class ProjectInitializer:
     def commit_changes(self, message):
         subprocess.run(['git', 'add', '.'], cwd=self.project_root)
         subprocess.run(['git', 'commit', '-m', message], cwd=self.project_root)
+    
+    
+    
+def main():
+    processor = TitanicDataProcessor('data/train_file.csv', 'data/test_file.csv')
+    processor.preprocess_data()
 
 if __name__ == '__main__':
     project = ProjectInitializer()
     project.initialize_project()
+    main()
+    
     
     tickets = [
         "Creation de la structure",
@@ -66,5 +130,3 @@ if __name__ == '__main__':
     subprocess.call(['git', 'add', '.'])
     subprocess.call(['git', 'commit', '-m', 'Ajout de tous les fichiers'])
     subprocess.call(['git', 'push'])
-
-    # Charger les données
